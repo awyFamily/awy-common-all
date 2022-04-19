b50b38b05c41f9cf8c80985b0fbbcfc228a23b34
yehongwei <yehongwei@yunyangdata.com>
2022-04-19 19:41:49 +0800

    完善认证组件

--- common-security-util/src/main/java/com/awy/common/security/oauth2/model/CustomizeAuthDTO.java
+++ common-security-util/src/main/java/com/awy/common/security/oauth2/model/CustomizeAuthDTO.java
@@ -1,19 +1,21 @@
 package com.awy.common.security.oauth2.model;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * custom auth query model
  * @author yhw
  * @date 2022-04-19
  */
+@NoArgsConstructor
 @Data
 public class CustomizeAuthDTO {
 
     /**
      * 自定义认证方法名称
      */
-    private final String customizeAuthName;
+    private String customizeAuthName;
 
     public CustomizeAuthDTO(String customizeAuthName) {
         this.customizeAuthName = customizeAuthName;
