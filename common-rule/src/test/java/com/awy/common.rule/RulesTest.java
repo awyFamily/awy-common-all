34ff0ea0f91534527e7e0577c895cd05bb4d34a5
yehongwei <yehongwei@yunyangdata.com>
2022-08-05 10:22:19 +0800

    完善规则组件测试

--- common-rule/src/test/java/com/awy/common.rule/RulesTest.java
+++ common-rule/src/test/java/com/awy/common.rule/RulesTest.java
@@ -64,10 +64,27 @@ public class RulesTest {
     }
 
     @Test
-    public void isSupportByGroup2Test() {
+    public void isSupportByGroupTest() throws Exception {
         //场景1(单个规则)： 当针对一个事件, n 分钟只允许触发 1 次
-        //        rules.registry(getTimerRuleModel());
+        rules.registry(getTimerRuleModel());
+        Assert.assertTrue(rules.isSupportByGroup("default","ABC",""));
+        //未超时,不满足
+        Assert.assertFalse(rules.isSupportByGroup("default","ABC",""));
+        //新的 key 满足
+        Assert.assertTrue(rules.isSupportByGroup("default","BCD",""));
+        //未超时,不满足
+        TimeUnit.SECONDS.sleep(2);
+        Assert.assertFalse(rules.isSupportByGroup("default","ABC",""));
+        //超时,满足
+        TimeUnit.SECONDS.sleep(2);
+        Assert.assertTrue(rules.isSupportByGroup("default","ABC",""));
+        //超时,满足
+        Assert.assertTrue(rules.isSupportByGroup("default","BCD",""));
 
+    }
+
+    @Test
+    public void isSupportByGroup2Test() {
         //场景2(多个规则组合使用)： 在物联网场景下，例如空气温度达到了 20 °, 会触发一条告警, 下一次告警需要在上次告警基础上 上下浮动 5° 才进行下次告警, 一个时间段内，只允许告警 n 次(超过则不进行告警)
         rules.registry(getFixedNumberRuleModel());
         rules.registry(getFloatValueRuleModel());
@@ -201,10 +218,85 @@ public class RulesTest {
 
     @Test
     public void isSupportByGroup4Test() throws Exception {
-        rules.registry(getFloatValueRuleModel3());
-        rules.registry(getTimerRuleModel3());
+        //一段时间(例如一天)内最多3次，浮动值为5, 距离上一条规则(定时)超过30分钟
+        rules.registry(getFloatValueRuleModel4());
+        rules.registry(getTimerRuleModel4());
+        rules.registry(getFixedNumberRuleModel4());
+
+        //第一次执行满足条件
+        Map<String,String> floatMap = new HashMap<>();
+        floatMap.put("a","10");
+        Assert.assertTrue(rules.isSupportByGroup("default","ABC",JSONUtil.toJsonStr(floatMap)));
+
+        floatMap = new HashMap<>();
+        floatMap.put("a","10");
+        Assert.assertFalse(rules.isSupportByGroup("default","ABC",JSONUtil.toJsonStr(floatMap)));
+
+        floatMap = new HashMap<>();
+        floatMap.put("a","15");
+        Assert.assertFalse(rules.isSupportByGroup("default","ABC",JSONUtil.toJsonStr(floatMap)));
+
+        //超时满足条件
+        TimeUnit.SECONDS.sleep(4);
+        floatMap = new HashMap<>();
+        floatMap.put("a","15");
+        Assert.assertTrue(rules.isSupportByGroup("default","ABC",JSONUtil.toJsonStr(floatMap)));
+
+        floatMap = new HashMap<>();
+        floatMap.put("a","10");
+        Assert.assertFalse(rules.isSupportByGroup("default","ABC",JSONUtil.toJsonStr(floatMap)));
+
+        //超时满足条件
+        TimeUnit.SECONDS.sleep(4);
+        floatMap = new HashMap<>();
+        floatMap.put("a","10");
+        Assert.assertTrue(rules.isSupportByGroup("default","ABC",JSONUtil.toJsonStr(floatMap)));
+
+        //超过最大次数,不满足
+        TimeUnit.SECONDS.sleep(4);
+        floatMap = new HashMap<>();
+        floatMap.put("a","15");
+        Assert.assertFalse(rules.isSupportByGroup("default","ABC",JSONUtil.toJsonStr(floatMap)));
     }
 
+    private RuleModel getFixedNumberRuleModel4() {
+        FixedNumberRuleModel fixedNumberRuleModel = new FixedNumberRuleModel();
+        fixedNumberRuleModel.setTimeout(3000L);
+        fixedNumberRuleModel.setFixedNumber(3);
+
+        RuleModel model = new RuleModel();
+        model.setRuleType(RuleTypeEnum.FIXED_NUMBER.getId());
+        model.setName("1");
+        model.setPriority(1);
+        model.setExpand(JSONUtil.toJsonStr(fixedNumberRuleModel));
+        return model;
+    }
+
+    private RuleModel getFloatValueRuleModel4() {
+        FloatValueRuleModel floatValueRuleModel = new FloatValueRuleModel();
+        Map<String,String> floatMap = new HashMap<>();
+        floatMap.put("a","5");
+        floatValueRuleModel.setFloatValueMaps(JSONUtil.toJsonStr(floatMap));
+
+        RuleModel model = new RuleModel();
+        model.setRuleType(RuleTypeEnum.FLOAT_VALUE.getId());
+        model.setName("2");
+        model.setPriority(2);
+        model.setExpand(JSONUtil.toJsonStr(floatValueRuleModel));
+        return model;
+    }
+
+    private RuleModel getTimerRuleModel4() {
+        TimerRuleModel timerRuleModel = new TimerRuleModel();
+        timerRuleModel.setTimeout(3L);
+
+        RuleModel model = new RuleModel();
+        model.setRuleType(RuleTypeEnum.TIMER.getId());
+        model.setName("3");
+        model.setPriority(3);
+        model.setExpand(JSONUtil.toJsonStr(timerRuleModel));
+        return model;
+    }
 
 
 }
