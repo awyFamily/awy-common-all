37517b69c9bedb071fc53880453ad0ab5469ea8d
yehongwei <yehongwei@yunyangdata.com>
2022-08-05 14:22:30 +0800

    完善规则组件

--- common-rule/src/main/java/com/awy/common/rule/model/FixedNumberRuleModel.java
+++ common-rule/src/main/java/com/awy/common/rule/model/FixedNumberRuleModel.java
@@ -9,7 +9,30 @@ import lombok.Data;
 @Data
 public class FixedNumberRuleModel extends RuleConfigModel {
 
+    /**
+     *
+     * seconds
+     * minutes
+     * hours
+     * days
+     */
+    private String timeUnit = "seconds";
+
     private Long timeout = 60L;
 
+    public Long getTimeout() {
+        if ("seconds".equalsIgnoreCase(this.timeUnit)) {
+            return this.timeout;
+        } else if ("minutes".equalsIgnoreCase(this.timeUnit)) {
+            return this.timeout * 60;
+        } else if ("hours".equalsIgnoreCase(this.timeUnit)) {
+            return this.timeout * 60 * 60;
+        } else if ("days".equalsIgnoreCase(this.timeUnit)) {
+            return this.timeout * 60 * 60 * 24;
+        }
+        return this.timeout;
+    }
+
     private Integer fixedNumber = 3;
+
 }
